9eb5f51a8c716a1a2ed2b5e23132f7b8eb424700
rasheed <47017579+RasheedMirzaa@users.noreply.github.com>
2019-03-18 16:54:11 +0700

    Add files via upload

---
+++ vue_router/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Rasheed Mirza Maulana</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 2</td>
+      <td>Cristian Ronaldo</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 3</td>
+      <td>Lionel Messi</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
